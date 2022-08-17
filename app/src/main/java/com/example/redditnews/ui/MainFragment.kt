fb8edc188dd16813c4f4ca4dc38e8fb251145160
bai_777 <bai_777@mail.ru>
2022-08-17 20:17:53 +0300

    сдаю дз

--- app/src/main/java/com/example/redditnews/ui/MainFragment.kt
+++ app/src/main/java/com/example/redditnews/ui/MainFragment.kt
@@ -1,21 +1,68 @@
 package com.example.redditnews.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.redditnews.R
+import android.widget.AbsListView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.redditnews.databinding.FragmentMainBinding
+import com.example.redditnews.domain.RedditPost
 
 
 class MainFragment : Fragment() {
 
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
+
+    private val viewModel: MainViewModel by lazy {
+        ViewModelProvider(this).get(MainViewModel::class.java)
+    }
+
+    private val adapter = MainScreenRecyclerAdapter()
+
+    var isScrolling = false
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+    ): View {
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initRecycler()
+    }
+
+    private fun initRecycler() {
+        val observer = Observer<List<RedditPost>> {
+            adapter.setData(it)
+        }
+        val manager = LinearLayoutManager(requireContext())
+        binding.recycler.layoutManager = manager
+        binding.recycler.adapter = adapter
+        viewModel.getData().observe(viewLifecycleOwner, observer)
+        viewModel.requestPosts()
+        binding.recycler.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+                if (newState == AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL) isScrolling =
+                    true
+            }
+
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                if (isScrolling && (manager.childCount + manager.findFirstVisibleItemPosition() >= manager.itemCount - 3)) {
+                    isScrolling = false
+                    viewModel.requestPosts()
+                }
+            }
+        })
     }
 }
