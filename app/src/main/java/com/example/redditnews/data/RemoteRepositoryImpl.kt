e9bcd27c6a112f305357b4774b339e478f082566
bai_777 <bai_777@mail.ru>
2022-08-17 19:49:22 +0300

    создал Repository.kt, ApiResponse.kt, ApiService.kt, RedditPost.kt, RemoteRepositoryImpl.kt

---
+++ app/src/main/java/com/example/redditnews/data/RemoteRepositoryImpl.kt
@@ -0,0 +1,16 @@
+package com.example.redditnews.data
+
+import com.example.redditnews.data.network.ApiService
+import com.example.redditnews.domain.RedditPost
+import io.reactivex.rxjava3.core.Single
+
+class RemoteRepositoryImpl(private val repo: ApiService) : Repository {
+    var after: String = ""
+
+    override fun getPosts(): Single<List<RedditPost>> {
+        return repo.getPosts(after).map {
+            this.after = it.data.after
+            convertResponseToPosts(it)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/redditnews/domain/RedditPost.kt
@@ -0,0 +1,4 @@
+package com.example.redditnews.domain
+
+class RedditPost {
+}
